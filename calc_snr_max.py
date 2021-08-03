d4972ccd99fd021f8ac1832d4d082a8e1069338d
Tahamid Siam <usiam@u.rochester.edu>
2021-08-02 23:50:31 -0400

    new plots added

--- calc_snr_max.py
+++ calc_snr_max.py
@@ -1,16 +1,24 @@
 import os
 os.sys.path.append('./utils/')
-from utils.objects import load_object, storage_object
+from utils.objects import load_object
 from utils.load_inputs import fill_data
 from utils.make_plots import MakePlots
+import numpy as np
 
 if __name__ == '__main__':
     # load inputs
-    configfile = 'calc_snr_max.cfg'
+    configfile = 'hirax_snr_sodium.cfg'
     so = load_object(configfile)
     cload = fill_data(so)
 
     make_plots = MakePlots(so)
-    # make_plots.plot_exo_doppler(savefig=False)
-    # make_plots.plot_stel_exo_doppler(savefig=False)
-    make_plots.plot_hirax_over_spectra(savefig=True)
+    # make_plots.plot_example_spectra(savefig=True)
+    # make_plots.plot_no_doppler(savefig=True)
+    # make_plots.plot_exo_doppler(savefig=True)
+    # make_plots.plot_stel_exo_doppler(savefig=True)
+    # make_plots.plot_hirax_over_shifted_exo(savefig=True)
+    # make_plots.plot_hirax_over_spectra(savefig=True)
+    # make_plots.plot_lorentz_fit(savefig=True, feature=configfile.split('_')[2].split('.')[0])
+    make_plots.plot_noise_mag(cload, magnitudes=np.arange(7, 17, 1), savefig=True)
+
+
