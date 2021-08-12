1aa5fdbb71c4ee9825f40b78d6ac47bfdaf83430
Tahamid Siam <usiam@u.rochester.edu>
2021-08-12 16:06:31 -0400

    minor comments were made

--- calc_snr_max.py
+++ calc_snr_max.py
@@ -5,20 +5,23 @@ from utils.load_inputs import fill_data
 from utils.make_plots import MakePlots
 import numpy as np
 
+
 if __name__ == '__main__':
     # load inputs
-    configfile = 'hirax_snr_sodium.cfg'
+    configfile = 'hirax_snr_potassium.cfg'
     so = load_object(configfile)
     cload = fill_data(so)
 
-    make_plots = MakePlots(so)
+    # make_plots = MakePlots(so)
     # make_plots.plot_example_spectra(savefig=True)
+    # make_plots.plot_hirax_profile(savefig=True)
     # make_plots.plot_no_doppler(savefig=True)
     # make_plots.plot_exo_doppler(savefig=True)
     # make_plots.plot_stel_exo_doppler(savefig=True)
-    # make_plots.plot_hirax_over_shifted_exo(savefig=True)
     # make_plots.plot_hirax_over_spectra(savefig=True)
+    # make_plots.plot_hirax_over_shifted_exo(savefig=True)
     # make_plots.plot_lorentz_fit(savefig=True, feature=configfile.split('_')[2].split('.')[0])
-    make_plots.plot_noise_mag(cload, magnitudes=np.arange(7, 17, 1), savefig=True)
+    # make_plots.plot_noise_mag(cload, magnitudes=np.arange(7, 17, 1), savefig=True)
+
 
 
--- hirax_snr_plots.py
+++ hirax_snr_plots.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from scipy.optimize import curve_fit
+import pandas as pd
 
 font = {'size': 8}
 matplotlib.rc('font', **font)
@@ -78,32 +79,50 @@ def fit_lorentz_aw(x_data, y_data, y_err, init_guess, feature = 'sodium'):
                            absolute_sigma=True)
     return -popt, pcov
 
+# Check out scipy.minimize to automate the code
+
 if __name__ == '__main__':
     # load inputs
-    configfile = 'hirax_snr_sodium.cfg'
+    configfile = 'hirax_snr_potassium.cfg'
     so = load_object(configfile)
     cload = fill_data(so)
 
-    magnitudes = np.arange(7, 17, 1)
-    tfac = np.arange(0.2 , 1, 0.2)
-    a = [[]*len(magnitudes) for i in range(len(tfac))]
-    a_err = [[]*len(magnitudes) for i in range(len(tfac))]
-    w = [[]*len(magnitudes) for i in range(len(tfac))]
-    w_err = [[]*len(magnitudes) for i in range(len(tfac))]
-    default = so.var.vmag
-    for ind,fac in enumerate(tfac):
-        for mag in magnitudes:
-            so.var.vmag = mag
-            cload.stellar(so)
-            cload.hirax(so, fac)
-            ratio, error_in_ratio = functions.calc_flux_ratio(so)
-            popt, pcov = fit_lorentz_aw(so.hirax.center_lam, ratio, y_err=error_in_ratio, init_guess=[-0.00035, 3], feature=configfile.split('_')[2].split('.')[0])
-            a[ind].append(popt[0]/np.sqrt(pcov[0][0]))
-            w[ind].append(popt[1] / np.sqrt(pcov[1][1]))
-            a_err[ind].append(np.sqrt(pcov[0][0]))
-            w_err[ind].append(np.sqrt(pcov[1][1]))
-    so.var.vmag = default
+    mag = np.arange(7, 17, 1)
+    tfactor = np.arange(0.2 , 1, 0.2)
 
-    make_plots = MakePlots(so)
-    make_plots.plot_SNR_mag_tput(magnitudes=magnitudes, amplitudes=a, tfac = np.arange(0.2 , 1, 0.2), savefig=True)
+    def calc_amplitude_width(magnitudes, tfac):
+        a = [[]*len(magnitudes) for i in range(len(tfac))]
+        a_err = [[]*len(magnitudes) for i in range(len(tfac))]
+        w = [[]*len(magnitudes) for i in range(len(tfac))]
+        w_err = [[]*len(magnitudes) for i in range(len(tfac))]
+        default = so.var.vmag
+        for ind,fac in enumerate(tfac):
+            for mag in magnitudes:
+                so.var.vmag = mag
+                cload.stellar(so)
+                cload.hirax(so, fac)
+                ratio, error_in_ratio = functions.calc_flux_ratio(so)
+                popt, pcov = fit_lorentz_aw(so.hirax.center_lam, ratio, y_err=error_in_ratio, init_guess=[-0.00035, 3], feature=configfile.split('_')[2].split('.')[0])
+                a[ind].append(popt[0])
+                w[ind].append(popt[1])
+                a_err[ind].append(np.sqrt(pcov[0][0]))
+                w_err[ind].append(np.sqrt(pcov[1][1]))
 
+        so.var.vmag = default
+        return np.array(a), np.array(a_err), abs(np.array(w)), np.array(w_err)
+
+    def write_snr_data(amplitudes, amplitude_errs, widths, width_errs, magnitudes, tfac):
+        mydict = {
+            'magnitude': magnitudes,
+        }
+        for i in range(len(tfac)):
+            mydict[f'amplitude_tfac{i+1}'] = amplitudes[i]
+            mydict[f'amplitude_err_tfac{i+1}'] = amplitude_errs[i]
+            mydict[f'width_tfac{i+1}'] = widths[i]
+            mydict[f'width_err_tfac{i+1}'] = width_errs[i]
+        df = pd.DataFrame(mydict)
+        df.to_csv(f"data/output/amplitude_vmag_{so.hirax.hirax_file.split('_')[2].split('.')[0]}.csv", sep=',', index=False)
+
+    a, a_err, w, w_err = calc_amplitude_width(magnitudes = mag, tfac = tfactor)
+    snr = a/a_err
+    write_snr_data(amplitudes=a, amplitude_errs=a_err, widths=w, width_errs=w_err, magnitudes=mag, tfac=tfactor)
